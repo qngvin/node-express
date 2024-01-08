70979b8f3bbd22a1409fb1b83f7133eced1d8bfb
quangvinh <quangvinh11602@gmail.com>
2024-01-08 10:56:37 +0700

    Express Router

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 http = require("http");
 const hostname = "localhost";
 const port = 3000;
+const nationRouter = require("./routes/nationRouter");
+
 const bodyParser = require("body-parser");
 const app = express();
-const morgan = require("morgan");
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+// const morgan = require("morgan");
+// app.use(morgan("dev"));
+// app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+app.use("/nations", nationRouter);
 // app.use((req, res, next) => {
 //   console.log(req.headers);
 //   res.statusCode = 200;
@@ -15,58 +18,6 @@ app.use(bodyParser.json());
 //   res.end("<html><body><h1>Hello Word</h1></body></html>");
 // });
 
-app.all("/nations", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
-
-app.get("/nations", (req, res, next) => {
-  res.end("Will send all the nations to you!");
-});
-
-app.post("/nations", (req, res, next) => {
-  res.end(
-    "Will add the nation: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-
-app.put("/nations", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /nations");
-});
-
-app.delete("/nations", (req, res, next) => {
-  res.end("Deleting all nations");
-});
-
-app.get("/nations/:nationId", (req, res, next) => {
-  res.end(
-    "Will send details of the nation: " + req.params.nationId + " to you!"
-  );
-});
-
-app.post("/nations/:nationId", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("POST operation not supported on /nations/" + req.params.nationId);
-});
-
-app.put("/nations/:nationId", (req, res, next) => {
-  res.write("Updating the nation: " + req.params.nationId + "\n");
-  res.end(
-    "Will update the nation: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-
-app.delete("/nations/:nationId", (req, res, next) => {
-  res.end("Deleting nation: " + req.params.nationId);
-});
 
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
